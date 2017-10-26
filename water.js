d4b2b19e76f6e414156362837062bd8e9e868da9
ISChetverikov <ischetverikov@gmail.com>
2017-10-26 22:13:42 +0300

    Deleted methods for testing, moved declaration on Heap and Point into main function

--- water.js
+++ water.js
@@ -2,6 +2,75 @@
 
 function get_water_volume(island) {
     
+    function Point(height, x, y) {
+        this.height = height;
+        this.x = x;
+        this.y = y;
+    };
+
+    // Реализация min-heap кучи
+    function MinHeap(fieldName) {
+        var data = [];
+        var indexedFieldName = fieldName;
+                
+        // Метод добавления элемента в кучу
+        this.enqueue = function(obj) {
+            data.push(obj);
+          
+            var index = data.length - 1;
+            while (index > 0) {
+                var parent = Math.floor((index + 1) / 2) - 1;
+                
+                if (data[parent][indexedFieldName] > data[index][indexedFieldName]) {
+                      var temp = data[parent];
+                      data[parent] = data[index];
+                      data[index] = temp;
+                }
+                else
+                    break;
+                
+                index = parent;
+            }
+        };
+        
+        // Метод извлечения минимального элемента
+        this.dequeue = function() {
+            if(data.length <= 0){
+                return null;
+            }
+            if(data.length == 1){
+                return data.pop();
+            }
+            
+            var min = data[0];
+            data[0] = data.pop();
+            
+            var index = 0;
+            while (true){
+                var left = 2 * index + 1;
+                var right = left + 1;
+                var smallestValueIndex = index;
+                
+                if (left < data.length && data[left][indexedFieldName] < data[smallestValueIndex][indexedFieldName])
+                    smallestValueIndex = left;
+                if (right < data.length && data[right][indexedFieldName] < data[smallestValueIndex][indexedFieldName])
+                    smallestValueIndex = right;
+                
+                if (smallestValueIndex != index){
+                    var temp = data[smallestValueIndex];
+                    data[smallestValueIndex] = data[index];
+                    data[index] = temp;
+                    
+                    index = smallestValueIndex
+                }
+                else {
+                    break;
+                }
+            }
+            return min;
+        };
+    };
+    
 	var minHeap = new MinHeap('height');
 	var M = island.length;
     var N = island[0].length;
@@ -46,87 +115,7 @@ function get_water_volume(island) {
     return waterVolume;
 };
 
-function Point(height, x, y) {
-    this.height = height;
-    this.x = x;
-    this.y = y;
-      
-    this.toString = function(){
-        return height + ":" +" ("+x+", "+y+")";
-    }
-};
 
-// Реализация min-heap кучи
-function MinHeap(fieldName) {
-    var data = [];
-    var indexedFieldName = fieldName;
-    
-  
-    this.toString = function(){
-        var str = "";
-        for (var i = 0; i < data.length; i++){
-            str += data[i].toString() + ", ";
-        }
-        return str;
-    };
-    
-    // Метод добавления элемента в кучу
-    this.enqueue = function(obj) {
-        data.push(obj);
-      
-        var index = data.length - 1;
-        while (index > 0) {
-            var parent = Math.floor((index + 1) / 2) - 1;
-            
-            if (data[parent][indexedFieldName] > data[index][indexedFieldName]) {
-                  var temp = data[parent];
-                  data[parent] = data[index];
-                  data[index] = temp;
-            }
-            else
-                break;
-            
-            index = parent;
-        }
-    };
-    
-    // Метод извлечения минимального элемента
-    this.dequeue = function() {
-        if(data.length <= 0){
-            return null;
-        }
-        if(data.length == 1){
-            return data.pop();
-        }
-        
-        var min = data[0];
-        data[0] = data.pop();
-        
-        var index = 0;
-        while (true){
-            var left = 2 * index + 1;
-            var right = left + 1;
-            var smallestValueIndex = index;
-            
-            if (left < data.length && data[left][indexedFieldName] < data[smallestValueIndex][indexedFieldName])
-                smallestValueIndex = left;
-            if (right < data.length && data[right][indexedFieldName] < data[smallestValueIndex][indexedFieldName])
-                smallestValueIndex = right;
-            
-            if (smallestValueIndex != index){
-                var temp = data[smallestValueIndex];
-                data[smallestValueIndex] = data[index];
-                data[index] = temp;
-                
-                index = smallestValueIndex
-            }
-            else {
-                break;
-            }
-        }
-        return min;
-    };
-};
 
 
 
