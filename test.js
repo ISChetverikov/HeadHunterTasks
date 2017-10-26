d64b73a027b1d8fb01767534b107d01757b57a7b
ISChetverikov <ischetverikov@gmail.com>
2017-10-26 18:09:10 +0300

    Implementation binary min heap and some tests for it

---
+++ test.js
@@ -0,0 +1,48 @@
+describe("MinHeap", function() {
+
+  function makeTest(elementsCount) {
+    var minHeap = new MinHeap();
+    var testData = [];
+    for(var i = 0; i < elementsCount; i++){
+        testData.push(Math.floor(Math.random() * 1000) + 1);
+    }
+    
+    for (var i = 0; i < testData.length; i++){
+        minHeap.enqueue(testData[i]);
+    }
+    
+    var comparator = function(a, b){
+        return a - b;
+    }
+    testData.sort(comparator);
+    
+    var extractTestData = [];
+    for(var i = 0; i < testData.length; i++){
+        extractTestData.push(minHeap.dequeue());
+    }
+    
+    it("Проверка отсортированности извлекаемых значений\n\r" + extractTestData.join(", "), function() {
+        for(var i = 0; i < testData.length; i++){
+            assert.equal(extractTestData[i], testData[i], extractTestData[i]+" равно "+testData[i]);
+        }
+    });
+  }
+
+  function emptyHeapDequeue(){
+      var minHeap = new MinHeap();
+      var element = minHeap.dequeue();
+      
+      it("При попытке извлечения из пустой непользованной кучи вернется " + element, function() {
+         assert.isNull(element);
+      });
+  }
+  
+  emptyHeapDequeue();
+  for(var i = 0; i < 100; i++){
+    var count = Math.floor(Math.random() * 250) + 1;
+    makeTest(count);
+  }
+    
+  
+  
+});
--- water.js
+++ water.js
@@ -1,8 +1,78 @@
 
 
-alert(get_water_volume(5));
-
 function get_water_volume(island) {
     
-    return 0;
-}
+	var minHeap = new MinHeap();
+	
+    minHeap.enqueue(17);
+    minHeap.enqueue(15);
+    minHeap.enqueue(13);
+    minHeap.enqueue(10);
+    minHeap.enqueue(2);
+    minHeap.enqueue(4);
+    minHeap.enqueue(6);
+    
+    return minHeap.show();
+};
+
+function MinHeap() {
+    var data = [];
+    
+    this.enqueue = function(val) {
+        data.push(val);
+      
+        var index = data.length - 1;
+        while (index > 0) {
+            var parent = Math.floor((index + 1) / 2) - 1;
+            
+            if (data[parent] > data[index]) {
+                  var temp = data[parent];
+                  data[parent] = data[index];
+                  data[index] = temp;
+            }
+            else
+                break;
+            
+            index = parent;
+        }
+    };
+    
+    this.dequeue = function() {
+        if(data.length <= 0){
+            return null;
+        }
+        if(data.length == 1){
+            return data.pop();
+        }
+        
+        var min = data[0];
+        data[0] = data.pop();
+        
+        var index = 0;
+        while (true){
+            var left = 2 * index + 1;
+            var right = left + 1;
+            var smallest = index;
+            
+            if (left < data.length && data[left] < data[smallest])
+                smallest = left;
+            if (right < data.length && data[right] < data[smallest])
+                smallest = right;
+            
+            if (smallest != index){
+                var temp = data[smallest];
+                data[smallest] = data[index];
+                data[index] = temp;
+                
+                index = smallest
+            }
+            else {
+                break;
+            }
+        }
+        return min;
+    };
+};
+
+
+
